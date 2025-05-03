86883f022f261ca4be559bed1c018d4b8df21bae
Metellus <metellusjunior56@gmail.com>
2025-05-03 15:11:39 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
 
 // Write your code here
+
+const num1 = 31
+const num2 = 2
+let multiply = num1 * num2
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const max = Math.max(1, 2, 3, 4, 20);
+
+const num3=8
+const num4=2
+const mod = num3/ num4
